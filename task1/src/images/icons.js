e2e8929a07d851c125765bf36483ed33d6642ce4
38097 <vlad_pavlyuk_@ukr.net>
2022-09-27 17:11:32 +0300

    add images

---
+++ task1/src/images/icons.js
@@ -0,0 +1,12 @@
+const icons = {
+    "Idea": require("../Icons/idea-icon.png"),
+    "Quote": require("../Icons/quote-icon.png"),
+    "Task": require("../Icons/task-icon.png"),
+    "Random thought": require("../Icons/random-thought-icon.png"),
+    "delete": require('../Icons/delete-icon.png'),
+    "edit": require('../Icons/edit-icon.png'),
+    "archive": require('../Icons/archive-icon.png'),
+    "unarchive": require('../Icons/unarchive-icon.png'),
+    "default": require("../Icons/default.png")
+}
+export default icons
